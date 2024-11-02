1101b7fa9a188987f701fd041a08e6efc161254e
Aldair Cosetito <aldair.loq.369@gmail.com>
2024-11-01 23:58:11 -0500

    Completed cube animation and move location from one point to another

--- Cubes-Animation/ViewController.swift
+++ Cubes-Animation/ViewController.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var cubeView: UIView!
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let actionTap = UITapGestureRecognizer(target: self, action: #selector(actionTapped))
         cubeView.isUserInteractionEnabled = true
-           cubeView.addGestureRecognizer(actionTap)
-        
+        cubeView.addGestureRecognizer(actionTap)
     }
     
     @objc func actionTapped() {
@@ -25,17 +23,14 @@ class ViewController: UIViewController {
     }
     
     func rotateImage(view: UIView) {
-        // Configurar la animación
         let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
         rotationAnimation.toValue = CGFloat.pi
         rotationAnimation.duration = 1.0
         rotationAnimation.fillMode = .forwards
         rotationAnimation.isRemovedOnCompletion = false
-        
-        // Aplicar la animación
         view.layer.add(rotationAnimation, forKey: "rotationAnimation")
     }
-
+    
     @IBAction func moveHereButtonTapped(_ sender: UIButton) {
         let xOffset = cubeView.center.x - sender.center.x
         let yOffset = ((cubeView.center.y + (sender.frame.height * 1.5)) - sender.center.y)
